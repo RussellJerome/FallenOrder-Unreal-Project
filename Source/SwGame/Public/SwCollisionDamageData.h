68939e894ea4f47f5a4ba751dd03ef954e120831
RussellJ <darkmanvoo@gmail.com>
2022-01-06 23:30:28 -0500

    Resetting up plugins

---
+++ Source/SwGame/Public/SwCollisionDamageData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SwCollisionDamageData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWGAME_API USwCollisionDamageData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
