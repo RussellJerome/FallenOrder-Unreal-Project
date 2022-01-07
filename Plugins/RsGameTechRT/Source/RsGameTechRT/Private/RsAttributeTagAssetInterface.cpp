0641070e19b3b8d386afabca590d0e3027b134e2
RussellJ <darkmanvoo@gmail.com>
2022-01-06 19:34:51 -0500

    Begining the Cleaning

--- Plugins/RsGameTechRT/Source/RsGameTechRT/Private/RsAttributeTagAssetInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "RsAttributeTagAssetInterface.h"
-
-// Add default functionality here for any IRsAttributeTagAssetInterface functions that are not pure virtual.
